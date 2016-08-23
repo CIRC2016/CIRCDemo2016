46c3ce38d69c29e9b05caba73559179520d32b86
ethan0150 <minghong2000@gmail.com>
2016-08-24 02:33:15 +0800

    merged part of David's rendering code

--- src/org/circ34/demo/Board.java
+++ src/org/circ34/demo/Board.java
@@ -1,57 +1,120 @@
-package org.circ34.demo;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.Math;
 
-public class Board extends JPanel implements ActionListener{
+/**
+ * Created by ethan on 2016/8/22.
+ */
+public class test {
+    private JPanel p1;
+    private JPanel p2;
+    private JButton playRetryButton;
+    private JButton scoreboardButton;
+    private JTable table1;
+    private Image bg,c1;
+    private int bgx=0,bgy=-2048+512;
+    private int c1x,c1y;
+    private boolean GameIsOver=false;
+    private byte PlayerCurrentColumn;
+    private Timer ScrollDelay;
 
-    private final int INTERVAL=40;
 
-    private Image bg;
-    private int bgx;
-    private int bgy;
+    public test() {
+        playRetryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(GameIsOver){
+                    initGame();
+                }
+                else{
+                    stopGame();
+                }
+            }
+        });
+        scoreboardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-    private Image p1;
-    private int p1x;
-    private int p1y;
+                //System.out.println("btn2");
+            }
+        });
 
-    private Timer timer;
+        p1.addKeyListener(new KeyAdapter() {
+            int keyvalue;
+            boolean IsScrolling=false,IsJumping=false;
+            @Override
+            public void keyPressed(KeyEvent e) {
+                keyvalue = e.getKeyCode();
+                switch (keyvalue) {
+                    case KeyEvent.VK_UP:
+                        if (!IsScrolling)ScrollDelay.start();
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        if(!IsJumping){
+                            if(PlayerCurrentColumn%2==1)
+                        }
+                        break;
+                    case KeyEvent.VK_RIGHT:
 
-    public Board(){
-        initBoard();
+                        break;
+                }
+            }
+            @Override
+            public void keyReleased(KeyEvent e){
+                keyvalue = e.getKeyCode();
+                switch (keyvalue){
+                    case KeyEvent.VK_UP:
+                        ScrollDelay.stop();
+                        break;
+                }
+            }
+        });
+        ScrollDelay=new Timer(40, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bgy++;
+                p1.repaint();
+            }
+        });
     }
 
-    private void initBoard(){
-        loadImage();
-        p1x=0;
-        p1y=768-200;
-        bgx=0;
-        bgy=-2048+768;
-        timer = new Timer(INTERVAL,this);
-        timer.start();
-    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("test");
+        frame.setContentPane(new test().p1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(768, 512);
+        frame.setTitle("CIRCDemo2016");
+        frame.setVisible(true);
 
-    private void loadImage(){
-        ImageIcon tmp = new ImageIcon("p1.png");
-        p1=tmp.getImage().getScaledInstance(100,100,Image.SCALE_SMOOTH);
-        tmp = new ImageIcon("back.png");
-        bg=tmp.getImage();
     }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(bg, bgx, bgy, null);
-        g.drawImage(p1, p1x, p1y,null);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (bgy<0){
-        bgy++;
+    private class rendering extends JPanel{
+        @Override
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            g.drawImage(bg,bgx,bgy,null);
+            g.drawImage(c1,c1x,c1y,null);
         }
-        repaint();
     }
+    private void SpawnBalls(){
+        int BallColumn=(((int)(1000*Math.random()))%7)+1;
 
+    }
+    private void initGame(){
+        ScrollDelay.stop();
+        ImageIcon tmp = new ImageIcon("c1.png");
+        c1=tmp.getImage();
+        tmp = new ImageIcon("bg.png");
+        bg=tmp.getImage();
+        bgx=0;
+        bgy=-2048+512;
+        c1x=0;
+        c1y=512-150;
+        PlayerCurrentColumn=1;
+    }
+    private void stopGame(){
+
+    }
 }
+
